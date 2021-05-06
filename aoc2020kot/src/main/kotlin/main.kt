d2246c4c0b57420d1c269be3ab9fbe4b776aa121
Ahmed Fasih <wuzzyview@gmail.com>
2021-05-06 00:33:28 -0700

    2a

--- aoc2020kot/src/main/kotlin/main.kt
+++ aoc2020kot/src/main/kotlin/main.kt
@@ -1,3 +1,62 @@
+// From https://stackoverflow.com/a/53018129
+fun getResourceAsText(path: String): String? {
+    return object {}.javaClass.getResource(path)?.readText()
+}
+
+fun getResourceAsInts(path: String): List<Int> {
+    val contents = getResourceAsText(path);
+    if (contents != null) {
+        return contents.trim().lines().map { it.toInt() }
+    }
+    throw Exception("File not found")
+}
+
+fun problem1a(): Pair<Int, List<Int>>? {
+    val targetSum = 2020
+    val expenses = getResourceAsInts("1.txt")
+    val seen: MutableSet<Int> = mutableSetOf()
+    for (x in expenses) {
+        if (seen.contains(targetSum - x)) {
+            return Pair((targetSum - x) * x, listOf(targetSum - x, x))
+        }
+        seen += x
+    }
+    return null
+}
+
+fun problem1b(): Pair<Int, List<Int>>? {
+    val targetSum = 2020
+    val expenses = getResourceAsInts("1.txt")
+    val seen: MutableSet<Int> = mutableSetOf()
+    for (x in expenses) {
+        for (y in seen) {
+            if (seen.contains(targetSum - (x + y))) {
+                return Pair(x * y * (targetSum - (x + y)), listOf(x, y, targetSum - (x + y)))
+            }
+        }
+        seen += x
+    }
+    return null
+}
+
+fun problem2a(): Int? {
+    val re = "([0-9]+)-([0-9]+) (.): (.+)".toRegex()
+    fun isValid(lo: Int, hi: Int, ch: String, pw: String): Boolean {
+        val count = ch.toRegex().findAll(pw).count()
+        return count in lo..hi
+    }
+
+    val contents = getResourceAsText("2.txt")
+    if (contents != null) {
+        return contents.trim().lineSequence().mapNotNull { re.find(it)?.destructured }
+            .filter { (lo, hi, ch, pw) -> isValid(lo.toInt(), hi.toInt(), ch, pw) }.count()
+    }
+    return null
+}
+
 fun main(args: Array<String>) {
+    println("Problem 1a: ${problem1a()}")
+    println("Problem 1b: ${problem1b()}")
+    println("Problem 2a: ${problem2a()}")
     println("Hello World!")
 }
